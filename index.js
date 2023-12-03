db6581cf5d4fff428ccd45021c98b93d271fd1c0
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2023-12-03 19:59:54 +0300

    done

--- index.js
+++ index.js
@@ -10,11 +10,11 @@
  */
 
 // Part 1 answer 👇🏻 ...
-
-
-
-
-
+let fullName = `Wahab Alawadhi`;
+let yearOfBirth = 1999;
+let hobby = `hiking`;
+let funFact = `im funny`;
+const image = "https://picsum.photos/200/301";
 
 /**
  * Part 2: String Interpolation
@@ -24,18 +24,16 @@
  * 1. fullNameString      -> assign it to: My name is {fullName}
  * 2. yearOfBirthString   -> assign it to: I am {YOUR_AGE}, and make sure you calculate your age from your year of birth
  * 3. hobbyString         -> assign it to: My hobby is {YOUR_HOBBY}
- * 
+ *
  * Uncomment the following lines and start interpolating.
  * You can uncomment a line by removing the // at the start
  */
-
-// const fullNameString = `My name is ${}`;
-// const yearOfBirthString =
-// const hobbyString =
-
-
-
-
+let currYear = 2023;
+let yourAge = currYear - yearOfBirth;
+const fullNameString = `My name is ${fullName}`;
+const yearOfBirthString = `My Year of birth is ${yourAge}`;
+const hobbyString = `My hobby is ${hobby}`;
+console.log(yourAge);
 /**
  * Part 3: Re-assignment
  * Increment your hacker score
@@ -45,14 +43,18 @@ let stars = 0;
 
 function incrementBy1() {
   // Increment stars by 1 👇🏻
+  stars += 1;
 }
 function decrementBy1() {
   // decrement stars by 1 👇🏻
+  stars -= 1;
 }
 
 function incrementBy2() {
   // Increment stars by 2 👇🏻
+  stars += 2;
 }
 function decrementBy2() {
   // decrement stars by 2 👇🏻
+  stars -= 2;
 }
